a772589d6ed6c78836bcdb4cdc2f4f4c0af78c3b
filolozka <hhshkola@gmail.com>
2020-11-17 18:26:03 +0100

    added fileService - reading from file to list of image descriptions objects

--- src/main/java/de/telran/processor/application/ImageProcessor.java
+++ src/main/java/de/telran/processor/application/ImageProcessor.java
@@ -3,26 +3,33 @@ package de.telran.processor.application;
 import de.telran.processor.entity.ImageDescriptor;
 import de.telran.processor.services.FileService;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ImageProcessor {
     private FileService fileService;
 
     public static void main(String[] args) {
+
         String csvFile = args[0];
 
         FileService fileService = new FileService();
         ImageProcessor processor = new ImageProcessor(fileService);
-        processor.process(csvFile);
-        //тут нужны mockito тесты
 
+        try {
+            processor.process(csvFile);
+        }
+        catch (IOException ex){
+            System.out.println(ex);
+        }
     }
 
     public ImageProcessor(FileService fileService){
         this.fileService = fileService;
     }
 
-    public void process(String fileName){
-        List<ImageDescriptor> imageDescriptors = fileService.readImageDescriptions(fileName);
+    public void process(String fileName) throws IOException{
+      List<ImageDescriptor> imageDescriptors = fileService.readImageDescriptions(fileName);
+        System.out.println(imageDescriptors);
     }
 }
--- src/main/java/de/telran/processor/services/FileService.java
+++ src/main/java/de/telran/processor/services/FileService.java
@@ -4,23 +4,35 @@ import de.telran.processor.entity.ImageDescriptor;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileService {
     public List<ImageDescriptor> readImageDescriptions(String fileName) {
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null)
-                System.out.println(line);
+        List<String> listOfImageDescriptions = readFileToStringList(fileName);
+        return listOfImageDescriptions.stream().map(this::convertStringToObject).collect(Collectors.toList());
+    }
+
+    private ImageDescriptor convertStringToObject(String line){
+        return new ImageDescriptor(line.split("::")[0], line.split("::")[1]);
+    }
+
+    private List<String> readFileToStringList(String fileName) {
+        List<String> stringList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            stringList = bufferedReader.lines().collect(Collectors.toList());
         } catch (Exception f) {
             System.out.println(f.getMessage());
         }
-        return Collections.EMPTY_LIST;
+        System.out.println(stringList);
+        return stringList;
     }
 
     public static void main(String[] args) {
         FileService fs = new FileService();
         fs.readImageDescriptions("images.csv");
+        List<ImageDescriptor> descriptors = new ArrayList<>(fs.readImageDescriptions("images.csv"));
+        System.out.println(descriptors.toString());
     }
 }
