c3982eaa60f50ec3c4b4cc6d14a867c89a4b3f92
filolozka <hhshkola@gmail.com>
2020-11-17 18:36:27 +0100

    added fileservice with reading to the list of objects

--- src/main/java/de/telran/processor/services/FileService.java
+++ src/main/java/de/telran/processor/services/FileService.java
@@ -10,7 +10,28 @@ import java.util.stream.Collectors;
 
 public class FileService {
     public List<ImageDescriptor> readImageDescriptions(String fileName) {
-        return new ArrayList<>();
+        List<String> listOfImageDescriptions = readFileToStringList(fileName);
+        return listOfImageDescriptions.stream().map(this::convertStringToObject).collect(Collectors.toList());
+    }
+    private ImageDescriptor convertStringToObject(String line){
+        return new ImageDescriptor(line.split("::")[0], line.split("::")[1]);
     }
 
+    private List<String> readFileToStringList(String fileName) {
+        List<String> stringList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            stringList = bufferedReader.lines().collect(Collectors.toList());
+        } catch (Exception f) {
+            System.out.println(f.getMessage());
+        }
+        System.out.println(stringList);
+        return stringList;
+    }
+
+    public static void main(String[] args) {
+        FileService fs = new FileService();
+        fs.readImageDescriptions("images.csv");
+        List<ImageDescriptor> descriptors = new ArrayList<>(fs.readImageDescriptions("images.csv"));
+        System.out.println(descriptors.toString());
+    }
 }
