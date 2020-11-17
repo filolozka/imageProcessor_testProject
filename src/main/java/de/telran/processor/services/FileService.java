a3a05ea3efc4c21d63b10e815a0134402bf62c42
filolozka <hhshkola@gmail.com>
2020-11-17 18:34:16 +0100

    deleted some files

--- src/main/java/de/telran/processor/services/FileService.java
+++ src/main/java/de/telran/processor/services/FileService.java
@@ -10,29 +10,7 @@ import java.util.stream.Collectors;
 
 public class FileService {
     public List<ImageDescriptor> readImageDescriptions(String fileName) {
-        List<String> listOfImageDescriptions = readFileToStringList(fileName);
-        return listOfImageDescriptions.stream().map(this::convertStringToObject).collect(Collectors.toList());
+        return new ArrayList<>();
     }
 
-    private ImageDescriptor convertStringToObject(String line){
-        return new ImageDescriptor(line.split("::")[0], line.split("::")[1]);
-    }
-
-    private List<String> readFileToStringList(String fileName) {
-        List<String> stringList = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            stringList = bufferedReader.lines().collect(Collectors.toList());
-        } catch (Exception f) {
-            System.out.println(f.getMessage());
-        }
-        System.out.println(stringList);
-        return stringList;
-    }
-
-    public static void main(String[] args) {
-        FileService fs = new FileService();
-        fs.readImageDescriptions("images.csv");
-        List<ImageDescriptor> descriptors = new ArrayList<>(fs.readImageDescriptions("images.csv"));
-        System.out.println(descriptors.toString());
-    }
 }
