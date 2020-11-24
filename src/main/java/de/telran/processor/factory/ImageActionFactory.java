0804929858fba33ed00f043693f81da3e05ab29a
filolozka <hhshkola@gmail.com>
2020-11-24 14:07:43 +0100

    delete unwanted changes

--- src/main/java/de/telran/processor/factory/ImageActionFactory.java
+++ src/main/java/de/telran/processor/factory/ImageActionFactory.java
@@ -15,30 +15,15 @@ import java.util.Map;
 import java.util.Properties;
 
 public class ImageActionFactory {
-    private ActionConfigServiceInt actionsConfigService;
-    private Map<String, ImageAction> imageActionMap = new HashMap<>();
-
-    public ImageActionFactory(ActionsConfigService configService) throws Exception {
-        this.actionsConfigService = configService;
-
-        List<String> actionClassNames = actionsConfigService.getActionClassNames();
-        String actionPackage = actionsConfigService.getActionPackage();
-
-        for (String actionClassName : actionClassNames) {
-            ImageAction imageAction = (ImageAction) Class.forName(actionPackage + "."
-                    + actionClassName).getConstructor().newInstance();
-            imageActionMap.put(imageAction.getName(), imageAction);
-        }
-    }
-
     public ImageAction getAction(String actionName) {
-        return imageActionMap.get(actionName);
-    }
-
-    public static void main(String[] args) throws Exception {
-        ImageActionFactory imageActionFactory = new ImageActionFactory(new ActionsConfigService());
-        ImageAction preview = imageActionFactory.getAction("PREVIEW");
-        preview.doAction(null);
+        switch (actionName) {
+            case "PREVIEW":
+                return new PreviewImageAction();
+            case "GRAYSCALE":
+                return new GrayscaleImageAction();
+            default:
+                return new DefaultImageAction();
+        }
     }
 }
 
