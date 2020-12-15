e83416d7363b4b53b79b7139ed8879eca3588740
filolozka <hhshkola@gmail.com>
2020-12-15 13:49:29 +0100

    added FileConfigService without test

---
+++ src/main/java/de/telran/processor/services/FileConfigService.java
@@ -0,0 +1,23 @@
+package de.telran.processor.services;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class FileConfigService {
+    private Properties property = new Properties();
+
+    public FileConfigService() throws Exception {
+        loadProperties();
+    }
+
+    public String getPathToSavedImages(String pathForSaveImages) {
+        return property.getProperty("pathForSaveImages");
+    }
+
+    private void loadProperties() throws IOException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = loader.getResourceAsStream("application.properties");
+        property.load(stream);
+    }
+}
